4aa492c515026f077c2fdd7a4fcdc2e656bc4c23
sw-private-demo <renedemo001@gmail.com>
2021-07-31 13:24:55 +0900

    Updates

---
+++ precache-manifest.5e9e9303db072259f4cd58291cc08af8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "483cf9df3e0f251e283b1dacf1845b22",
+    "url": "/ra4w1-hmi-demo/index.html"
+  },
+  {
+    "revision": "cb4a3a7310218f4065b4",
+    "url": "/ra4w1-hmi-demo/static/js/2.dd63596d.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/ra4w1-hmi-demo/static/js/2.dd63596d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb4357bbb245e196c41",
+    "url": "/ra4w1-hmi-demo/static/js/main.8f40a3d7.chunk.js"
+  },
+  {
+    "revision": "8d8ba98abdfb15693068",
+    "url": "/ra4w1-hmi-demo/static/js/runtime-main.fa2eb947.js"
+  },
+  {
+    "revision": "a931d56706bbd9ff507d3b6db5b5df68",
+    "url": "/ra4w1-hmi-demo/static/media/connect.a931d567.png"
+  },
+  {
+    "revision": "574b3f84131dd5e7c792c5c46be65199",
+    "url": "/ra4w1-hmi-demo/static/media/fan_high_speed.574b3f84.gif"
+  },
+  {
+    "revision": "6db14f9c5b332d6092c01d788cb03d32",
+    "url": "/ra4w1-hmi-demo/static/media/fan_low_speed.6db14f9c.gif"
+  },
+  {
+    "revision": "4c6ce8de10f8f268bfdda40f1b714ce0",
+    "url": "/ra4w1-hmi-demo/static/media/fan_normal_speed.4c6ce8de.gif"
+  },
+  {
+    "revision": "be44afbcb256e256dc0504537d64fbad",
+    "url": "/ra4w1-hmi-demo/static/media/fan_stop.be44afbc.png"
+  },
+  {
+    "revision": "63c63f8277723e4088a92fd23ee87399",
+    "url": "/ra4w1-hmi-demo/static/media/fan_swing.63c63f82.gif"
+  },
+  {
+    "revision": "42273ec0bebc6607ebb1d3fe22f7d6f2",
+    "url": "/ra4w1-hmi-demo/static/media/indoor_environment.42273ec0.png"
+  },
+  {
+    "revision": "57eaa6ccddc1c3f7205e0372b9551111",
+    "url": "/ra4w1-hmi-demo/static/media/led_control.57eaa6cc.png"
+  },
+  {
+    "revision": "1d889375f77f70242e0f6bd4e6bee651",
+    "url": "/ra4w1-hmi-demo/static/media/vr_result.1d889375.png"
+  },
+  {
+    "revision": "9a34a66c7e0f4aff64b440c2f89543f7",
+    "url": "/ra4w1-hmi-demo/static/media/vr_settings.9a34a66c.png"
+  }
+]);
